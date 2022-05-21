4c3cc686de92db528d851e9790190347223d6174
Daniel Jimenez <danielejimeneza@gmail.com>
2022-05-21 09:15:52 -0500

    Agregar archivos de proyecto.

---
+++ S7DJIMENEZB/Login.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using S7DJIMENEZB.Modelo;
+using SQLite;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S7DJIMENEZB
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        private SQLiteAsyncConnection connection;
+        public Login()
+        {
+            InitializeComponent();
+            connection = DependencyService.Get<Database>().GetConnection();
+        }
+
+        public static IEnumerable<Estudiante> Select_Where(SQLiteConnection db,string user, string password)
+        {
+            return db.Query<Estudiante>("SELECT * FROM Estudiante where Usuario = ? and Contraseña = ?", user, password);
+        }
+
+        private async void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uisrael.db3");
+                var db = new SQLiteConnection(databasePath);
+                db.CreateTable<Estudiante>();
+                IEnumerable<Estudiante> resultado = Select_Where(db, txtUsuario.Text, txtContraseña.Text);
+                if (resultado.Count() > 0)
+                {
+                    await Navigation.PushAsync(new ConsultaRegistro());
+                }
+                else
+                {
+                    await DisplayAlert("Error", "Usuario o contraseña incorrectos", "Cerrar");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ S7DJIMENEZB/Modelo/Estudiante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace S7DJIMENEZB.Modelo
+{
+    public class Estudiante
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string User { get; set; }
+        [MaxLength(50)]
+        public string Password { get; set; }
+    }
+}
